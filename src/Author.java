06523e527c196073c296700b3b72677e3605640b
5rshv <serishev.alesha@gmail.com>
2024-11-19 21:16:31 +0300

    initial commit

---
+++ src/Author.java
@@ -0,0 +1,24 @@
+public class Author {
+    String name = "Sarah";
+    String lastName = "Konor";
+
+    public Author (String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,27 @@
+import java.security.PublicKey;
+
+public class Book {
+    String author;
+    int age;
+
+    public Book(String Author, int age) {
+        this.author = Author;
+        this.age = age;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
